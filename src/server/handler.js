5a04ff3fe26b782adf036e1038bae79de2477f40
Prasinta Nadia <prasintanadia@gmail.com>
2024-12-08 01:32:53 +0700

    Add files via upload

---
+++ src/server/handler.js
@@ -0,0 +1,39 @@
+const predictClassification = require('../services/inferenceService');
+const crypto = require('crypto');
+const storeData = require('../services/storeData');
+
+async function postPredictHandler(request, h) {
+  const { image } = request.payload;
+  const { model } = request.server.app;
+
+  const { confidenceScore, label, explanation, suggestion } = await predictClassification(model, image);
+  const id = crypto.randomUUID();
+  const createdAt = new Date().toISOString();
+
+  let result = 'Cancer';
+  let mySuggestion = 'Segera periksa ke dokter!';
+
+  if (confidenceScore < 99) {
+    result = 'Non-cancer';
+    mySuggestion = 'Jaga kesehatan anda!';
+  }
+
+  const data = {
+    "id": id,
+    "result": result,
+    "suggestion": mySuggestion,
+    "createdAt": createdAt
+  }
+
+  await storeData(id,data);
+
+  const response = h.response({
+    status: 'success',
+    message: 'Model is predicted successfully',
+    data
+  })
+  response.code(201);
+  return response;
+}
+
+module.exports = postPredictHandler;
